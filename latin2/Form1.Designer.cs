68f0850a3d712254995bcfd6430b169d10c196ec
Volosii <lechakiss@gmail.com>
2023-12-09 13:55:25 +0300

    Добавьте файлы проекта.

---
+++ latin2/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace latin2
+{
+    partial class VolosiiLatinRussian
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VolosiiLatinRussian));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(189, 117);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(414, 38);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(189, 267);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(414, 38);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Location = new System.Drawing.Point(327, 323);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 62);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Perevod";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(321, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 31);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Piši Sjuda!";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(301, 220);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(207, 31);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Kopiruj Otsjuda!";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(62, 17);
+            this.radioButton1.TabIndex = 13;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Latinica";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 58);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(86, 17);
+            this.radioButton2.TabIndex = 14;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "FUN (Polskij)";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(12, 35);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(153, 17);
+            this.radioButton3.TabIndex = 15;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Standartnaja Transletiracija";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // VolosiiLatinRussian
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(782, 459);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "VolosiiLatinRussian";
+            this.Text = "VolosiiLatinApp";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.VolosiiLatinRussian_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ latin2/Form1.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace latin2
+{
+    public partial class VolosiiLatinRussian : Form
+    
+
+    {
+        Dictionary<string, string> words = new Dictionary<string, string>();
+        Dictionary<string, string> words2 = new Dictionary<string, string>();
+        Dictionary<string, string> ords = new Dictionary<string, string>();
+
+
+
+        public VolosiiLatinRussian()
+        {
+            InitializeComponent();
+            words.Add("а", "a");
+            words.Add("б", "b");
+            words.Add("в", "v");
+            words.Add("г", "g");
+            words.Add("д", "d");
+            words.Add("е", "e");
+            words.Add("ё", "jo");
+            words.Add("ж", "ž");
+            words.Add("з", "z");
+            words.Add("и", "i");
+            words.Add("й", "j");
+            words.Add("к", "k");
+            words.Add("л", "l");
+            words.Add("м", "m");
+            words.Add("н", "n");
+            words.Add("о", "o");
+            words.Add("п", "p");
+            words.Add("р", "r");
+            words.Add("с", "s");
+            words.Add("т", "t");
+            words.Add("у", "u");
+            words.Add("ф", "f");
+            words.Add("х", "h");
+            words.Add("ц", "c");
+            words.Add("ч", "č");
+            words.Add("ш", "š");
+            words.Add("щ", "ŝ");
+            words.Add("ъ", "j");
+            words.Add("ы", "y");
+            words.Add("ь", "'");
+            words.Add("э", "é");
+            words.Add("ю", "ju");
+            words.Add("я", "ja");
+            words.Add("А", "A");
+            words.Add("Б", "B");
+            words.Add("В", "V");
+            words.Add("Г", "G");
+            words.Add("Д", "D");
+            words.Add("Е", "E");
+            words.Add("Ё", "Jo");
+            words.Add("Ж", "Ž");
+            words.Add("З", "Z");
+            words.Add("И", "I");
+            words.Add("Й", "J");
+            words.Add("К", "K");
+            words.Add("Л", "L");
+            words.Add("М", "M");
+            words.Add("Н", "N");
+            words.Add("О", "O");
+            words.Add("П", "P");
+            words.Add("Р", "R");
+            words.Add("С", "S");
+            words.Add("Т", "T");
+            words.Add("У", "U");
+            words.Add("Ф", "F");
+            words.Add("Х", "H");
+            words.Add("Ц", "C");
+            words.Add("Ч", "Č");
+            words.Add("Ш", "Š");
+            words.Add("Щ", "Ŝ");
+            words.Add("Ъ", "J");
+            words.Add("Ы", "Y");
+            words.Add("Ь", "'");
+            words.Add("Э", "É");
+            words.Add("Ю", "Ju");
+            words.Add("Я", "Ja");
+
+            words2.Add("а", "а");
+            words2.Add("б", "b");
+            words2.Add("в", "v");
+            words2.Add("г", "g");
+            words2.Add("д", "d");
+            words2.Add("е", "e");
+            words2.Add("ё", "jo");
+            words2.Add("ж", "rz");
+            words2.Add("з", "z");
+            words2.Add("и", "i");
+            words2.Add("й", "j");
+            words2.Add("к", "k");
+            words2.Add("л", "l");
+            words2.Add("м", "m");
+            words2.Add("н", "n");
+            words2.Add("о", "o");
+            words2.Add("п", "p");
+            words2.Add("р", "r");
+            words2.Add("с", "s");
+            words2.Add("т", "t");
+            words2.Add("у", "u");
+            words2.Add("ф", "f");
+            words2.Add("х", "сh");
+            words2.Add("ц", "c");
+            words2.Add("ч", "cz");
+            words2.Add("ш", "sz");
+            words2.Add("щ", "szcz");
+            words2.Add("ъ", "j");
+            words2.Add("ы", "y");
+            words2.Add("ь", "'");
+            words2.Add("э", "é");
+            words2.Add("ю", "ju");
+            words2.Add("я", "ja");
+            words2.Add("А", "A");
+            words2.Add("Б", "B");
+            words2.Add("В", "V");
+            words2.Add("Г", "G");
+            words2.Add("Д", "D");
+            words2.Add("Е", "E");
+            words2.Add("Ё", "Jo");
+            words2.Add("Ж", "Rz");
+            words2.Add("З", "Z");
+            words2.Add("И", "I");
+            words2.Add("Й", "J");
+            words2.Add("К", "K");
+            words2.Add("Л", "L");
+            words2.Add("М", "M");
+            words2.Add("Н", "N");
+            words2.Add("О", "O");
+            words2.Add("П", "P");
+            words2.Add("Р", "R");
+            words2.Add("С", "S");
+            words2.Add("Т", "T");
+            words2.Add("У", "U");
+            words2.Add("Ф", "F");
+            words2.Add("Х", "Сh");
+            words2.Add("Ц", "C");
+            words2.Add("Ч", "Cz");
+            words2.Add("Ш", "Sz");
+            words2.Add("Щ", "Szcz");
+            words2.Add("Ъ", "J");
+            words2.Add("Ы", "Y");
+            words2.Add("Ь", "'");
+            words2.Add("Э", "É");
+            words2.Add("Ю", "Ju");
+            words2.Add("Я", "Ja");
+
+            ords.Add("а", "a");
+            ords.Add("б", "b");
+            ords.Add("в", "v");
+            ords.Add("г", "g");
+            ords.Add("д", "d");
+            ords.Add("е", "e");
+            ords.Add("ё", "yo");
+            ords.Add("ж", "zh");
+            ords.Add("з", "z");
+            ords.Add("и", "i");
+            ords.Add("й", "j");
+            ords.Add("к", "k");
+            ords.Add("л", "l");
+            ords.Add("м", "m");
+            ords.Add("н", "n");
+            ords.Add("о", "o");
+            ords.Add("п", "p");
+            ords.Add("р", "r");
+            ords.Add("с", "s");
+            ords.Add("т", "t");
+            ords.Add("у", "u");
+            ords.Add("ф", "f");
+            ords.Add("х", "h");
+            ords.Add("ц", "c");
+            ords.Add("ч", "ch");
+            ords.Add("ш", "sh");
+            ords.Add("щ", "sch");
+            ords.Add("ъ", "'");
+            ords.Add("ы", "y");
+            ords.Add("ь", "'");
+            ords.Add("э", "e");
+            ords.Add("ю", "yu");
+            ords.Add("я", "ya");
+            ords.Add("А", "A");
+            ords.Add("Б", "B");
+            ords.Add("В", "V");
+            ords.Add("Г", "G");
+            ords.Add("Д", "D");
+            ords.Add("Е", "E");
+            ords.Add("Ё", "Yo");
+            ords.Add("Ж", "Zh");
+            ords.Add("З", "Z");
+            ords.Add("И", "I");
+            ords.Add("Й", "J");
+            ords.Add("К", "K");
+            ords.Add("Л", "L");
+            ords.Add("М", "M");
+            ords.Add("Н", "N");
+            ords.Add("О", "O");
+            ords.Add("П", "P");
+            ords.Add("Р", "R");
+            ords.Add("С", "S");
+            ords.Add("Т", "T");
+            ords.Add("У", "U");
+            ords.Add("Ф", "F");
+            ords.Add("Х", "H");
+            ords.Add("Ц", "C");
+            ords.Add("Ч", "Ch");
+            ords.Add("Ш", "Sh");
+            ords.Add("Щ", "Sch");
+            ords.Add("Ъ", "'");
+            ords.Add("Ы", "Y");
+            ords.Add("Ь", "'");
+            ords.Add("Э", "E");
+            ords.Add("Ю", "Yu");
+            ords.Add("Я", "Ya");
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            string source = textBox1.Text;
+            if (radioButton1.Checked)
+            {
+
+
+                foreach (KeyValuePair<string, string> pair in words)
+                {
+                    source = source.Replace(pair.Key, pair.Value);
+                }
+                textBox2.Text = source;
+            }
+            else if (radioButton2.Checked)
+            {
+                foreach (KeyValuePair<string, string> pair in words2)
+                {
+                    source = source.Replace(pair.Key, pair.Value);
+                }
+                textBox2.Text = source;
+
+
+            }
+            else if (radioButton3.Checked)
+            {
+                foreach (KeyValuePair<string, string> pair in ords)
+                {
+                    source = source.Replace(pair.Key, pair.Value);
+                }
+                textBox2.Text = source;
+            }
+
+        }
+
+      
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void VolosiiLatinRussian_Load(object sender, EventArgs e)
+        {
+ 
+        }
+
+        public void radioButton1_CheckedChanged(object sender, EventArgs e)
+            
+        {
+            
+        }
+
+        public void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void VolosiiLatinRussian_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == (char)Keys.Enter)
+
+            {
+
+                button1_Click(button1, null);
+            }
+        }
+    }
+}
